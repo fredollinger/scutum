a5d8fc9bf8f0dfc832ef002d483890e97ef48b78
fred_ollinger <fred_ollinger@seektech.com>
2012-11-27 10:58:54 -0800

    Added stub to over ride context menu.

---
+++ scutview.cpp
@@ -0,0 +1,38 @@
+/*
+ * Scutum
+ *
+ * ScutView:
+ * 
+ * Over rides QWebView
+ *
+ * Copyright (C) 2012 Fred Ollinger
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "scutview.hpp"
+#include <QAction>
+#include <QDebug>
+#include <QWebView>
+
+namespace scutum{
+ScutView::ScutView(QWidget* pParent) 
+	: QWebView(pParent)
+{}
+
+/**
+ * Class destructor.
+ */
+ScutView::~ScutView(){}
+} // namespace scutum
