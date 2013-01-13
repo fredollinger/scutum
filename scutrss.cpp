ba7c1fc309efe888960a9f2053053b9d8bf4b7ae
Fred Ollinger <follinge@gmail.com>
2013-01-12 16:45:29 -0800

    Added stubs for rss reader and testing.

---
+++ scutrss.cpp
@@ -0,0 +1,106 @@
+/*
+ * Scutum
+ *
+ * JsonDelicous:
+ * 
+ * Parses JSON Data For Delicious
+ *
+ * Copyright (C) 2012 Fred Ollinger
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "jsondelicious.hpp"
+#include <QDebug>
+#include <QStringList>
+
+namespace scutum{
+
+JsonDelicious::JsonDelicious(const QString &json) {
+  parse(json);
+}
+
+JsonDelicious::JsonDelicious() {
+}
+
+JsonDelicious::~JsonDelicious() {}
+
+QStringList JsonDelicious::list(const QString &json) {
+  QStringList qlist = json.split("}");
+  return qlist;
+}
+
+QString JsonDelicious::element(const QString &element, const QString &line) {
+  QString res;
+  QString f = "\""+element+"\": \"";
+  QStringList qsl = line.split(f);
+
+  if (qsl.size() < 2) return res;
+
+  qsl = qsl[1].split("\"");
+
+  res = qsl[0];
+
+  // TODO: Get out the element here
+
+  return res;
+}
+
+void JsonDelicious::parse(const QString &text) {
+  QStringList qlist = list(text);
+  foreach (QString line, qlist){
+          Json json;
+          json.title = element("d", line);
+          json.url = element("u", line);
+          //qDebug() << __PRETTY_FUNCTION__ << json.url << " : " << json.title;
+          m_jsonlist.append(json);
+  }
+}
+
+const QStringList JsonDelicious::titles(){
+	QStringList qsl;
+  foreach (Json json, m_jsonlist){
+		qsl.append(json.title);
+	}
+	return qsl;
+}
+
+/* Given a title, return a Url */
+const QUrl JsonDelicious::url(const QString &title){
+  QUrl url;
+  foreach (Json json, m_jsonlist){
+    if ( json.title == title ) return json.url;
+  }
+  return url; 
+}
+
+const JsonTitleLink JsonDelicious::titlelinks(){
+	JsonTitleLink map;
+	foreach (Json json, m_jsonlist){
+    map[json.title] = json.url;
+  }
+  return map;
+}
+
+int JsonDelicious::size(){
+  m_jsonlist.size();
+}
+
+void JsonDelicious::setData(const QString &json) {
+  m_jsonlist.clear();
+  parse(json);
+}
+
+} // namespace scutum
+// Sat Dec 22 16:12:57 PST 2012
---
+++ scutrss.hpp
@@ -0,0 +1,72 @@
+/*
+ * Scutum
+ *
+ * JsonDelicious:
+ * 
+ * Parses delicious json files
+ *
+ * Copyright (C) 2012 Fred Ollinger
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __JSON_DELICIOUS__
+#define __JSON_DELICIOUS__
+
+#include <QDateTime>
+#include <QList>
+#include <QString>
+#include <QUrl>
+
+namespace scutum{
+typedef QMap<QString, QString> JsonTitleLink;
+class JsonDelicious {
+public:
+  JsonDelicious(void);
+  JsonDelicious(const QString&);
+  ~JsonDelicious();
+
+  /* From a line, get an element */
+  QString element(const QString &element, const QString &line);
+
+  /* Given a QString, put this into a JsonList Struct */
+  void parse(const QString&);
+
+  void setData(const QString&);
+
+  int size(void); // how many items do we hold?
+
+  /* Given a QString, break into a QString List */
+  QStringList list(const QString&);
+
+  const QUrl url(const QString &title);
+
+	const QStringList titles(void);
+
+	const JsonTitleLink titlelinks(void);
+
+private:
+  struct Json{
+    QString title;
+    QString tag;
+    QString url;
+    QDateTime dt;
+  };
+  typedef QList<Json> JsonList;
+  JsonList m_jsonlist;
+};
+
+} // namespace scutum
+#endif // __JSON_DELICIOUS__
+// Mon Dec 17 18:13:13 PST 2012
